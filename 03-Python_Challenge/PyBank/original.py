56377f7b315271f57e6b8c975f43bae581ee03e5
Hunter Cash <hmclaug@gmail.com>
2019-02-24 21:58:04 -0600

    changes to pybank and pypoll

--- 03-Python_Challenge/PyBank/original.py
+++ 03-Python_Challenge/PyBank/original.py
@@ -7,11 +7,10 @@ from statistics import mean
 title = "Financial Analysis"
 lines = "----------------------------"
 
-greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999999999999999999]
-
 #open and read csv file
 csv_file = os.path.join("Resources", "budget_data.csv")
+# File path for txt document
+text_file_output = os.path.join("analysis.txt")
 
 # Open and read csv
 with open(csv_file, newline="") as csvfile:
@@ -47,22 +46,44 @@ with open(csv_file, newline="") as csvfile:
     
     #figure out average changes in profit and losses over entire period
     average_of_changes_list = []
-    for ind in range(len(p_l_total)):
-        if ind == 0:
+    for x in range(len(p_l_total)):
+        if x == 0:
             pass
         else:
-           average_of_changes_list.append(p_l_total[ind] - p_l_total[ind - 1])
+           average_of_changes_list.append(p_l_total[x] - p_l_total[x - 1])
     average_change = mean(average_of_changes_list)
-    print(average_of_changes_list)
-
-
-
-
+    
+    max_change = max(average_of_changes_list)
+    min_change = min(average_of_changes_list)
+    
+    max_change_date = str(date_list[average_of_changes_list.index(max(average_of_changes_list))])
+    
+    
+    min_change_date = str(date_list[average_of_changes_list.index(min(average_of_changes_list))])
     #print(average_of_changes_list)
     
     #print total of objects in list
     print(title)
     print(lines)
     print("Total Months: " + str(length_of_date_list))
-    print("Total: $" + str(p_l_total_sum))
+    print("Total Profit/Losses: $" + str(p_l_total_sum))
     print("Average Change: $" + str(float(round(average_change, 2))))
+    print("Greatest Increase in Revenue: " + str(max_change_date) + " " + "($" + str(max_change) + ")")
+    print("Greatest Decrease in Revenue: " + str(min_change_date) + " " + "($" + str(min_change) + ")")
+
+space = "\n"
+# Create a text Document
+with open(text_file_output, "w") as txt_file:
+    txt_file.write(title)
+    txt_file.write(space)
+    txt_file.write(lines)
+    txt_file.write(space)
+    txt_file.write("Total Months: " + str(length_of_date_list))
+    txt_file.write(space)
+    txt_file.write("Total Profit/Losses: $" + str(p_l_total_sum))
+    txt_file.write(space)
+    txt_file.write("Average Change: $" + str(float(round(average_change, 2))))
+    txt_file.write(space)
+    txt_file.write("Greatest Increase in Revenue: " + str(max_change_date) + " " + "($" + str(max_change) + ")")
+    txt_file.write(space)
+    txt_file.write("Greatest Decrease in Revenue: " + str(min_change_date) + " " + "($" + str(min_change) + ")")
